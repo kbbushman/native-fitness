b3d2fb0c88fcb4fdc715057abf0b2dabfc6fcbc8
kbbushman <k.bushman@yahoo.com>
2018-11-21 12:30:22 -0800

    Initial commit, adds boilerplate and basic add entry view

---
+++ components/AddEntry.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { getMetricMetaInfo, timeToString } from '../utils/helpers';
+import NativeSlider from './NativeSlider';
+import NativeStepper from './NativeStepper';
+import DateHeader from './DateHeader';
+import { Ionicons } from '@expo/vector-icons';
+import TextButton from './TextButton';
+import { submitEntry, removeEntry } from '../utils/api';
+
+const SubmitBtn = ({ onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
+
+class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0,
+  };
+
+  increment = metric => {
+    const { max, step } = getMetricMetaInfo(metric);
+
+    this.setState(state => {
+      const count = state[metric] + step;
+      return {
+        ...state,
+        [metric]: count > max ? max : count,
+      }
+    });
+  };
+
+  decrement = metric => {
+    this.setState(state => {
+      const count = state[metric] - getMetricMetaInfo(metric).step;
+      return {
+        ...state,
+        [metric]: count < 0 ? 0 : count,
+      }
+    });
+  };
+
+  slide = (metric, value) => {
+    this.setState(() => ({
+      [metric]: value,
+    }));
+  };
+
+  submit = () => {
+    const key = timeToString();
+    const entry = this.state;
+    // Update Redux
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0,
+    }));
+    // Navigate to home
+    submitEntry({ key, entry })
+    // Clear local notification
+  }
+
+  reset = () => {
+    const key = timeToString();
+    // Update Redux
+    // Route to home
+    removeEntry(key);
+  }
+
+  render() {
+    const metaInfo = getMetricMetaInfo();
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons name='ios-happy' size={100} />
+          <Text>You Already logged your information for today</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
+
+    return (
+      <View style={{marginTop: '15%', marginLeft: '5%'}}>
+        <DateHeader date={(new Date).toLocaleDateString()} />
+        {Object.keys(metaInfo).map(key => {
+          const { getIcon, type, ...rest } = metaInfo[key];
+          const value = this.state[key];
+
+          return (
+            <View key={key}>
+              {getIcon()}
+              {type === 'slider'
+                ? <NativeSlider
+                    value={value}
+                    onChange={(value) => this.slide(key, value)}
+                    {...rest}
+                  />
+                : <NativeStepper
+                    value={value}
+                    onIncrement={() => this.increment(key)}
+                    onDecrement={() => this.decrement(key)}
+                    {...rest}
+                  />}
+            </View>
+          )
+        })}
+        <SubmitBtn onPress={this.submit} />
+      </View>
+    );
+  };
+}
+
+export default AddEntry;
---
+++ components/NativeSlider.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, Slider } from 'react-native';
+
+const NativeSlider = ({ max, unit, step, value, onChange }) => {
+  return (
+    <View>
+      <Slider
+        step={step}
+        value={value}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}
+      />
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default NativeSlider;
